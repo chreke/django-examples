51d27786f7b865bb4ccec533a3f173665b513974
Christoffer Ekeroth <ekeroth.christoffer@gmail.com>
2022-02-13 09:18:42 +0100

    Add minimum value to price field

--- csvupload/bulk/models.py
+++ csvupload/bulk/models.py
@@ -1,7 +1,12 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
     sku = models.CharField(max_length=16, unique=True)
-    price = models.DecimalField(max_digits=9, decimal_places=2)
+    price = models.DecimalField(
+        max_digits=9,
+        decimal_places=2,
+        validators=[MinValueValidator(0)]
+    )
     description = models.TextField()

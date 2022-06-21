73ed482ee8f1188cc8735f5b70eddfa379dac76e
Christoffer Ekeroth <ekeroth.christoffer@gmail.com>
2022-06-21 06:43:24 +0200

    Add Artist and Album models

--- musicdb/musicdb/models.py
+++ musicdb/musicdb/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Artist(models.Model):
+    name = models.CharField(max_length=32)
+
+
+class Album(models.Model):
+    artist = models.ForeignKey(
+        Artist,
+        related_name="albums",
+        on_delete=models.CASCADE,
+    )
+    title = models.CharField(max_length=32)
+    genre = models.CharField(max_length=16)
+    release_date = models.DateField()
+    length = models.DurationField()

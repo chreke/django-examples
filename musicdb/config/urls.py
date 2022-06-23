70c6dc0ac8b1f3527be5928845a63fa89c6df64c
Christoffer Ekeroth <ekeroth.christoffer@gmail.com>
2022-06-23 06:46:28 +0200

    Add URLs for artists and albums

--- musicdb/config/urls.py
+++ musicdb/config/urls.py
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework.routers import DefaultRouter
+
+from musicdb.views import AlbumViewSet, ArtistViewSet
+
+router = DefaultRouter()
+router.register("albums", AlbumViewSet)
+router.register("artists", ArtistViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+] + router.urls

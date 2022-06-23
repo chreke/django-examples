bc291eb899e951b8bee86fd39d4079e1728911e5
Christoffer Ekeroth <ekeroth.christoffer@gmail.com>
2022-06-23 06:46:46 +0200

    Add __str__ method for artists and albums

--- musicdb/musicdb/models.py
+++ musicdb/musicdb/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class Artist(models.Model):
     name = models.CharField(max_length=32)
 
+    def __str__(self):
+        return self.name
+
 
 class Album(models.Model):
     artist = models.ForeignKey(
@@ -15,3 +18,6 @@ class Album(models.Model):
     genre = models.CharField(max_length=16)
     release_date = models.DateField()
     length = models.DurationField()
+
+    def __str__(self):
+        return f"{self.title} ({self.release_date.year})"

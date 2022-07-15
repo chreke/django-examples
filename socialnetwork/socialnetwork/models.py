f8ca1945da7d384f5a3eb4a86d100ec426668cc3
Christoffer Ekeroth <ekeroth.christoffer@gmail.com>
2022-07-15 21:48:34 +0200

    Add User model

--- socialnetwork/socialnetwork/models.py
+++ socialnetwork/socialnetwork/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+	username = models.CharField(max_length=32, unique=True)
+	followers = models.ManyToManyField("User", related_name="following")
+
+	def __str__(self):
+		return self.username

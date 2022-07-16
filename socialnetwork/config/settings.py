667551fde0b0ddecab058fc095b32356084a2781
Christoffer Ekeroth <ekeroth.christoffer@gmail.com>
2022-07-16 08:24:45 +0200

    Auto-generate secret key on startup

--- socialnetwork/config/settings.py
+++ socialnetwork/config/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
+import secrets
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d2$#eyheki25b&fx_2*f1515&m^96cj6w9+pmp_%@g))kly+bt'
+SECRET_KEY = os.getenv("SECRET_KEY", secrets.token_urlsafe())
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

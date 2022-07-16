aeb54c54a495c58c0e6c34e23e56700e0f168f5c
Christoffer Ekeroth <ekeroth.christoffer@gmail.com>
2022-07-16 07:58:48 +0200

    Add "followers" action to UserViewSet

--- socialnetwork/socialnetwork/views.py
+++ socialnetwork/socialnetwork/views.py
@@ -1,3 +1,5 @@
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from .models import User
 from .serializers import UserSerializer
@@ -7,3 +9,9 @@ class UserViewSet(ModelViewSet):
     serializer_class = UserSerializer
     queryset = User.objects.all()
     lookup_field = "username"
+
+    @action(detail=True, methods=["get"])
+    def followers(self, request, username):
+        user = self.get_object()
+        serializer = self.get_serializer(user.followers, many=True)
+        return Response(serializer.data)

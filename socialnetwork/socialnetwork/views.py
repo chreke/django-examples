ca50b1bdc4a70c880a59e621cb83c642d85230f9
Christoffer Ekeroth <ekeroth.christoffer@gmail.com>
2022-07-16 07:05:07 +0200

    Add a viewset for Users

--- socialnetwork/socialnetwork/views.py
+++ socialnetwork/socialnetwork/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .models import User
+from .serializers import UserSerializer
 
-# Create your views here.
+
+class UserViewSet(ModelViewSet):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    lookup_field = "username"

d028ba3b0aa317cef268e02612b1719a6d2c81c9
Christoffer Ekeroth <ekeroth.christoffer@gmail.com>
2022-07-18 08:02:19 +0200

    Use SlugField for username instead of CharField

--- socialnetwork/socialnetwork/models.py
+++ socialnetwork/socialnetwork/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class User(models.Model):
-	username = models.CharField(max_length=32, unique=True)
+	username = models.SlugField(unique=True)
 	followers = models.ManyToManyField("User", related_name="following")
 
 	def __str__(self):

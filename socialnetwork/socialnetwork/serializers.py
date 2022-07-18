47ca2043308d8c9b627bb62e486fefaa224ea3e8
Christoffer Ekeroth <ekeroth.christoffer@gmail.com>
2022-07-18 07:45:44 +0200

    Add link to "followers" from serializer

--- socialnetwork/socialnetwork/serializers.py
+++ socialnetwork/socialnetwork/serializers.py
@@ -1,10 +1,17 @@
 # socialnetwork/serializers.py
 
 from .models import User
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import (
+    HyperlinkedIdentityField,
+    ModelSerializer,
+)
 
 
 class UserSerializer(ModelSerializer):
+    followers = HyperlinkedIdentityField(
+        view_name="user-followers",
+        lookup_field="username",
+    )
 
     class Meta:
         model = User

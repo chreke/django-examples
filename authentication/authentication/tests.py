c425e472eb59791f2bbea326863a087dbdcab315
Christoffer Ekeroth <ekeroth.christoffer@gmail.com>
2022-08-09 07:23:51 +0200

    Add test for creating users

--- authentication/authentication/tests.py
+++ authentication/authentication/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+class CreateAccountTest(APITestCase):
+
+    def test_create_account(self):
+        response = self.client.post(
+            "/accounts/",
+            {
+                "username": "foobar",
+                "password": "Supersecret!@#$",
+            }
+        )
+        self.assertEqual(response.status_code, 201)

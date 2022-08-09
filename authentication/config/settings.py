4114337987c15810830e67b737728b82dea7ab73
Christoffer Ekeroth <ekeroth.christoffer@gmail.com>
2022-08-09 06:51:45 +0200

    Remove hardcoded secret key

--- authentication/config/settings.py
+++ authentication/config/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
+import secrets
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@e-m05$e+=r%=v)$2@m(%2il1t^u1cfvf4y%(6a6)7#8n&m7@g'
+SECRET_KEY = os.getenv("SECRET_KEY", secrets.token_urlsafe())
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

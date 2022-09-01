6e553dd4d481353eef7c576bbafd88d9c4a4258f
Christoffer Ekeroth <ekeroth.christoffer@gmail.com>
2022-09-01 05:31:54 +0200

    Remove unused DefaultRouter

--- authentication/config/urls.py
+++ authentication/config/urls.py
@@ -1,11 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken.views import ObtainAuthToken
 
 from authentication import views
-
-router = DefaultRouter()
 
 urlpatterns = [
     path("admin/", admin.site.urls),

c5698a628dfb6fc761ecd27f07e0c92ab9607d9a
Christoffer Ekeroth <ekeroth.christoffer@gmail.com>
2022-09-01 06:11:45 +0200

    Change CORS_ALLOWED_ORIGINS setting

--- authentication/config/settings.py
+++ authentication/config/settings.py
@@ -130,7 +130,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:8080",
+    "http://localhost:3000",
 ]
 
 REST_FRAMEWORK = {

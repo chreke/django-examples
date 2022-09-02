a14bc36719625afbb34cb6fe7202c1303ac0deb4
Christoffer Ekeroth <ekeroth.christoffer@gmail.com>
2022-09-02 05:44:53 +0200

    Use session authentication for Browsable API
    The Browsable API works with sessions as well, which is preferable to
    Basic Auth

--- authentication/config/settings.py
+++ authentication/config/settings.py
@@ -136,12 +136,9 @@ CORS_ALLOWED_ORIGINS = [
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.TokenAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
     ],
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated",
     ]
 }
-if DEBUG:
-    REST_FRAMEWORK["DEFAULT_AUTHENTICATION_CLASSES"].insert(
-        0, "rest_framework.authentication.BasicAuthentication"
-    )
--- authentication/config/urls.py
+++ authentication/config/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from rest_framework.authtoken.views import ObtainAuthToken
 
 from authentication import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
     path("accounts/", views.CreateUserView.as_view()),
     path("sessions/", ObtainAuthToken.as_view()),
     path("me/", views.MeView.as_view()),
